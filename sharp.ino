09c67f2116606f215eff1c737e03ad1f3ee754fd
tunataskintuna35 <tunataskintuna35@gmail.com>
2015-12-28 22:34:06 +0200

    Update sharp.ino

--- sharp.ino
+++ sharp.ino
@@ -2,29 +2,43 @@ int sharp1=A0;
 int sharp2=A1;
 int sharp3=A2;
 
-#define VOLTS_PER_UNIT    .0049F        // (.0049 for 10 bit A-D) 
+float _sharp1;
+float _sharp2;
+float _sharp3;
 float volts;
 float cm;
+
+#define VOLTS_PER_UNIT    .0049F        // (.0049 for 10 bit A-D) 
+
 
 void setup()
 {
   
-  Serial.begin()
- 
- 
- 
+  Serial.begin(9600);
 }
-
-
 void loop()
 {
- volts = (float)proxSens * VOLTS_PER_UNIT; // ("proxSens" is from analog read)
- 
- cm = 60.495 * pow(volts,-1.1904);     // same in cm
- if (volts < .2) inches = -1.0;        // out of range  
+  
+        _sharp1=analogRead(sharp1);
+        _sharp2=analogRead(sharp2);
+        _sharp3=analogRead(sharp3);
+  
+        serial.println("Sensor1 value:");
+        serial.println(_sharp1);
+        serial.println("Sensor2 value:");
+        serial.println(_sharp2);
+        serial.println("Sensor3 value:");
+        serial.println(_sharp3);
+        volts = (float)proxSens * VOLTS_PER_UNIT; // ("proxSens" is from analog read)
+        cm = 60.495 * pow(volts,-1.1904);     // same in cm
  
  
 }
 
-
+float lenght(float voltage)
+{
+ 
+    
+  
+}
  
