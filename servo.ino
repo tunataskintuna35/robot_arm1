eff040ab228af8f9099f2a8c94b9fef40e971dba
tunataskintuna35 <tunataskintuna35@gmail.com>
2015-12-29 22:59:27 +0200

    Update servo.ino

--- servo.ino
+++ servo.ino
@@ -30,7 +30,7 @@ void gripper_on()
   for (pos = 0; pos <= 180; pos += 1) { // goes from 0 degrees to 180 degrees
     // in steps of 1 degree
     myservo.write(pos);              // tell servo to go to position in variable 'pos'
-    delay(15);                       // waits 15ms for the servo to reach the position
+    delay(30);                       // waits 30ms for the servo to reach the position
   }
 }
 
@@ -38,7 +38,7 @@ void gripper_off()
 {
    for (pos = 180; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
     myservo.write(pos);              // tell servo to go to position in variable 'pos'
-    delay(15);                       // waits 15ms for the servo to reach the position
+    delay(30);                       // waits 30ms for the servo to reach the position
   }
 }
 void arm_up()
@@ -46,7 +46,7 @@ void arm_up()
   for (int temp = 180; temp >= 0; temp -= 1) 
   { // goes from 180 degrees to 0 degrees
     servo2.write(temp);              // tell servo to go to position in variable 'pos'
-    delay(15);                       // waits 15ms for the servo to reach the position
+    delay(30);                       // waits 30ms for the servo to reach the position
   }
 }
 void arm_down()
@@ -54,6 +54,6 @@ void arm_down()
   for (int temp = 0; temp <= 180; temp += 1) 
   { // goes from 180 degrees to 0 degrees
     servo2.write(temp);              // tell servo to go to position in variable 'pos'
-    delay(15);                       // waits 15ms for the servo to reach the position
+    delay(30);                       // waits 30ms for the servo to reach the position
   }
 }
