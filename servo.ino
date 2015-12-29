a31857bbcb20cab85acaaf87ecefdc91cdf8a12f
tunataskintuna35 <tunataskintuna35@gmail.com>
2015-12-29 22:26:13 +0200

    Update servo.ino

--- servo.ino
+++ servo.ino
@@ -1,22 +1,51 @@
 #include <Servo.h>
 
-Servo myservo;  // create servo object to control a servo
-// twelve servo objects can be created on most boards
+Servo myservo;
+Servo servo2;
+
 
 int pos = 0;    // variable to store the servo position
 
-void setup() {
-  myservo.attach(10);  // attaches the servo on pin 9 to the servo object
+void setup() 
+{
+  myservo.attach(10);
 }
 
-void loop() {
+void loop() 
+{
+  
+ 
+}
+
+void gripper_on()
+{
   for (pos = 0; pos <= 180; pos += 1) { // goes from 0 degrees to 180 degrees
     // in steps of 1 degree
     myservo.write(pos);              // tell servo to go to position in variable 'pos'
     delay(15);                       // waits 15ms for the servo to reach the position
   }
-  for (pos = 180; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
+}
+
+void gripper_off()
+{
+   for (pos = 180; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
     myservo.write(pos);              // tell servo to go to position in variable 'pos'
+    delay(15);                       // waits 15ms for the servo to reach the position
+  }
+}
+void arm_up()
+{
+  for (int temp = 180; temp >= 0; temp -= 1) 
+  { // goes from 180 degrees to 0 degrees
+    servo2.write(temp);              // tell servo to go to position in variable 'pos'
+    delay(15);                       // waits 15ms for the servo to reach the position
+  }
+}
+void arm_down()
+{
+  for (int temp = 0; temp <= 180; temp += 1) 
+  { // goes from 180 degrees to 0 degrees
+    servo2.write(temp);              // tell servo to go to position in variable 'pos'
     delay(15);                       // waits 15ms for the servo to reach the position
   }
 }
